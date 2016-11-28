f5c4c5f3a3a477e8490f03071400d690df804e3f
Michael Calasanz <darthmdc@gmail.com>
2016-11-27 16:53:42 -0800

    Initial Commit

---
+++ ReadingXML.Webform/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ReadingXML.Webform.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <asp:GridView ID="GridView1" runat="server">
+                </asp:GridView>
+            </div>
+            <div>
+                <asp:TextBox ID="TextBox1" runat="server" Height="98px" TextMode="MultiLine" Width="280px"></asp:TextBox>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <asp:Button ID="btnReadXml" runat="server" Text="Read XML" OnClick="btnReadXml_Click" /></li>
+                    <li>
+                        <asp:Button ID="btnReadSchema" runat="server" Text="Read Schema" OnClick="btnReadSchema_Click" /></li>
+                </ul>
+            </div>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ ReadingXML.Webform/Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ReadingXML.DbData;
+using ReadingXML.MockData;
+using ReadingXML.Presenter;
+using ReadingXML.Presenter.Infrastructure;
+
+namespace ReadingXML.Webform
+{
+    public partial class Default : System.Web.UI.Page, IForm1View
+    {
+        IAuthors mockDb;
+        Form1Presenter _presenter;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            mockDb = new MockAuthors();
+            _presenter = new Form1Presenter(this, mockDb);
+        }
+
+        protected void btnReadXml_Click(object sender, EventArgs e)
+        {
+            _presenter.GetAuthors();
+        }
+
+        protected void btnReadSchema_Click(object sender, EventArgs e)
+        {
+            _presenter.GetSchema();
+        }
+
+        public System.Data.DataSet Authors
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                GridView1.DataSource = value;
+                GridView1.DataBind();
+            }
+        }
+
+        public string Schema
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                TextBox1.Text = value;
+            }
+        }
+    }
+}
